559fa44d100ceceb0827bc396d90a1d4e18399a4
Никита Россиин <n.rossiin98@yandex.ru>
2023-03-12 21:04:54 +0300

    Создание страницы профиля и отдельной игры

--- games_mark/games/urls.py
+++ games_mark/games/urls.py
@@ -7,4 +7,6 @@ app_name = 'games'
 urlpatterns = [
     path('', views.index, name='index'),
     path('category/<slug:slug>/', views.category_gamemark, name='category_gamemark'),
+    path('profile/<str:username>/', views.profile, name='profile'),
+    path('games/<int:game_id>/', views.game_detail, name='game_detail'),
 ]
--- games_mark/games/views.py
+++ games_mark/games/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Avg
 
 from games.models import User, Game, Category, GameMark
 
@@ -19,3 +20,23 @@ def category_gamemark(request, slug):
         'gamemark_list': gamemark_list,
     }
     return render(request, 'games/category_gamemark.html', context)
+
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    gamemarks_by_user = GameMark.objects.filter(user=user)
+    context = {
+        'user': user,
+        'gamemarks_by_user': gamemarks_by_user,
+    }
+    return render(request, 'games/profile.html', context)
+
+
+def game_detail(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+    gamemarks_by_game = GameMark.objects.filter(game=game).aggregate(Avg('mark'))
+    context = {
+        'game': game,
+        'gamemarks_by_game': gamemarks_by_game['mark__avg'],
+    }
+    return render(request, 'games/game_detail.html', context) 
